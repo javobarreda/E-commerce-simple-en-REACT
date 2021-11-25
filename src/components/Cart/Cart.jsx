cde1ee85e507e9f29196106bdb008037e24a9add
Javier Barreda Joo <javierbarredajoo@hotmail.com>
2021-11-25 02:47:24 -0500

    primera parte de formulario addres

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -28,7 +28,7 @@ const Cart = ( {cart, handleUpdateCartQty, handleRemoveFromCart, handleEmptyCart
             <Typography variant="h4">
                 Subtotal: {cart.subtotal.formatted_with_symbol}</Typography>
                 <div>
-                    <Button className={classes.emptyButton} size="large" type="button" variant="contained" color="secondary" onClick={handleEmptyCart} >Carro vacío</Button>
+                    <Button className={classes.emptyButton} size="large" type="button" variant="contained" color="secondary" onClick={handleEmptyCart} >Vaciar carrito</Button>
                     <Button component={Link} to="/checkout" className={classes.checkoutButton} size="large" type="button" variant="contained" color="primary">Checkout</Button>
                 </div>
 
--- src/components/CheckoutForm/Checkout/Checkout.jsx
+++ src/components/CheckoutForm/Checkout/Checkout.jsx
@@ -1,6 +1,8 @@
 import React, {useState} from 'react'
 import {Paper, Stepper, Step, StepLabel, Typography, CircularProgress, Divider, Button} from '@material-ui/core';
 import useStyles from './styles';
+import AddressForm from '../AddressForm';
+import PaymentForm from '../PaymentForm';
 
 //El stepper se mueve mientras nosotros avanzamos, es como el seguidos de cada paso.
 const steps = ['Shipping address', 'Payment details'];
@@ -8,7 +10,18 @@ const steps = ['Shipping address', 'Payment details'];
 const Checkout = () => {
     const [activeStep, setActiveStep] = useState(0);
     const classes = useStyles();
-    
+
+    const Confirmation = () => (
+        <div>
+            Confirmación
+        </div>
+
+    )
+
+    const Form = () => activeStep === 0
+        ? <AddressForm />
+        : <PaymentForm />
+    //el addres from y el payment estan listos para el primer paso, ellos estan cambiando. Luego el confirmation viene cuando pasemos estos pasos
     return (
         <>
            <div className={classes.toolbar} /> 
@@ -22,6 +35,7 @@ const Checkout = () => {
                             </Step>
                         ))}
                     </Stepper>
+                    {activeStep === steps.length ? <Confirmation /> : <Form />}
                 </Paper>
             </main>
         </>
